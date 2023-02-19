a8540135ef49295f3b3dc00536785525766c4b0a
satohan\satomu <nekochain9901@gmail.com>
2023-02-19 15:39:18 +0900

    #6 投稿のカテゴリ機能を追加

--- nuko_atume_project/app/views.py
+++ nuko_atume_project/app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from .models import Post
+from .models import Post, Category
 from .forms import PostForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class IndexView(View):
+    '''
+    ホーム画面に関する処理
+    '''
 
     def get(self, request, *args, **kwargs):
         '''
@@ -17,6 +20,9 @@ class IndexView(View):
 
 
 class PostDetailView(View):
+    '''
+    投稿の詳細画面に関する処理
+    '''
 
     def get(self, request, *args, **kwargs):
         '''
@@ -28,6 +34,9 @@ class PostDetailView(View):
 
 
 class CreatePostView(LoginRequiredMixin, View):
+    '''
+    新規投稿に関する処理
+    '''
 
     def get(self, request, *args, **kwargs):
         '''
@@ -39,14 +48,21 @@ class CreatePostView(LoginRequiredMixin, View):
 
     def post(self, request, *args, **kwargs):
         '''
-        新規投稿に関する処理
+        フォームにデータがあれば --> 詳細画面を呼び出す
+        フォームにデータがなければ --> 投稿フォームの画面を呼び出す
         '''
         form = PostForm(request.POST or None)
 
-        if form.is_valid():
+        if form.is_valid():  # フォームにデータがあるか確認
             post_data = Post()
             post_data.author = request.user
             post_data.title = form.cleaned_data['title']
+
+            category = form.cleaned_data['category']  # ホームから入力されたカテゴリーを習得
+            category_data = Category.objects.get(
+                name=category)  # データを上記で取得したカテゴリーでフィルタを掛けて取得する
+            post_data.category = category_data
+
             post_data.content = form.cleaned_data['content']
             post_data.save()  # 作成した投稿内容をデータベースに保存する
             return redirect('post_detail', post_data.id)
@@ -56,16 +72,20 @@ class CreatePostView(LoginRequiredMixin, View):
 
 
 class PostEditView(LoginRequiredMixin, View):
+    '''
+    投稿の編集画面に関する処理
+    '''
+
     def get(self, request, *args, **kwargs):
         '''
-        投稿の詳細画面を呼び出す
+        投稿フォーム画面を呼び出す
         '''
         post_data = Post.objects.get(id=self.kwargs['pk'])  # 特定の投稿を取得する
         form = PostForm(request.POST or None,
                         initial={'title': post_data.title,
+                                 'category': post_data.category,
                                  'content': post_data.content
-                                 }
-                        )
+                                 })
         return render(request, 'app/post_form.html',
                       {'form': form})
 
@@ -77,6 +97,12 @@ class PostEditView(LoginRequiredMixin, View):
         if form.is_valid():
             post_data = Post.objects.get(id=self.kwargs['pk'])
             post_data.title = form.cleaned_data['title']
+
+            category = form.cleaned_data['category']  # ホームから入力されたカテゴリーを習得
+            category_data = Category.objects.get(
+                name=category)  # データを上記で取得したカテゴリーでフィルタを掛けて取得する
+            post_data.category = category_data
+
             post_data.content = form.cleaned_data['content']
             post_data.save()  # 作成した投稿内容をデータベースに保存する
             return redirect('post_detail', self.kwargs['pk'])
@@ -86,6 +112,10 @@ class PostEditView(LoginRequiredMixin, View):
 
 
 class PostDeleteView(LoginRequiredMixin, View):
+    '''
+    投稿の削除に関する処理
+    '''
+
     def get(self, request, *args, **kwargs):
         '''
         投稿の削除画面を呼び出す
@@ -101,3 +131,12 @@ class PostDeleteView(LoginRequiredMixin, View):
         post_data = Post.objects.get(id=self.kwargs['pk'])  # 特定の投稿を取得する
         post_data.delete()
         return redirect('index')
+
+
+class CategoryView(View):
+    def get(self, request, *args, **kwargs):
+        category_data = Category.objects.get(name=self.kwargs['category'])
+        post_data = Post.objects.order_by('-id').filter(category=category_data)
+        return render(request, 'app/index.html', {
+            'post_data': post_data
+        })
